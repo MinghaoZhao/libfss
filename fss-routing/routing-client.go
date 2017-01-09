2c3a8a5071f34915d1b2a55c77968dc7bf86e496
Cathie Yun <cath_yun@mit.edu>
2017-01-09 12:35:31 -0800

    renamed files

--- fss-routing/routing-client.go
+++ fss-routing/routing-client.go
@@ -1,39 +1,20 @@
 package main
 
 import (
+  "net/http"
+  "io/ioutil"
   "fmt"
-  "net"
-  "bufio"
-  "../libfss"
-  "encoding/gob"
-)
-
-const (
-    CONN_HOST = "localhost"
-    CONN_PORT = "3333"
-    CONN_TYPE = "tcp"
+  "encoding/json"
 )
 
 func main() {
-  // Generate fss Keys on client
-  fClient := libfss.ClientInitialize(6)
-  // Test with if x = 10, evaluate to 2
-  fssKeys := fClient.GenerateTreePF(10, 2)
-
-  conn, _ := net.Dial(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
-  enc := gob.NewEncoder(conn)
-  enc.Encode(fssKeys)
-
-/*
-  // send to socket
-  fmt.Fprintf(conn, "message to send\n")
-
-  // listen for reply 
-  message, _ := bufio.NewReader(conn).ReadString('\n')
-  fmt.Print("Message from server: "+message)
-*/
-  
-  // Close the connection when you're done with it.
-  conn.Close()
-  fmt.Println("done");
+  resp, err := http.Get("http://localhost:8000/nodes/1234")
+  if err != nil {
+    // handle error
+  }
+  defer resp.Body.Close()
+  body, err := ioutil.ReadAll(resp.Body)
+  var node map[string]interface{}
+  err = json.Unmarshal(body, &node)
+  fmt.Println(node)
 }
--- fss-routing/routing-server.go
+++ fss-routing/routing-server.go
@@ -1,53 +1,27 @@
 package main
 
 import (
-  "fmt"
-  "net"
   "os"
-  // "../libfss"
-)
-
-const (
-    CONN_HOST = "localhost"
-    CONN_PORT = "3333"
-    CONN_TYPE = "tcp"
+  "gopkg.in/gin-gonic/gin.v1"
 )
 
 func main() {
-
-    // Listen for incoming connections.
-    l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
-    if err != nil {
-        fmt.Println("Error listening:", err.Error())
-        os.Exit(1)
+    port := os.Getenv("PORT")
+    if (port == "") {
+      port = "8000"
     }
-    // Close the listener when the application closes.
-    defer l.Close()
-    fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
-    for {
-        // Listen for an incoming connection.
-        conn, err := l.Accept()
-        if err != nil {
-            fmt.Println("Error accepting: ", err.Error())
-            os.Exit(1)
-        }
-        // Handle connections in a new goroutine.
-        go handleRequest(conn)
-    }
-}
-
-// Handles incoming requests.
-func handleRequest(conn net.Conn) {
-  // Make a buffer to hold incoming data.
-  buf := make([]byte, 1024)
-  // Read the incoming connection into the buffer.
-  _, err := conn.Read(buf)
-  if err != nil {
-    fmt.Println("Error reading:", err.Error())
-  }
-  fmt.Println("Message received.\n")
-  // Send a response back to person contacting us.
-  conn.Write([]byte("Message received.\n"))
-  // Close the connection when you're done with it.
-  conn.Close()
+    r := gin.Default()
+    r.GET("/nodes/:key", func(c *gin.Context) {
+        c.JSON(200, gin.H{
+            "key": c.Param("key"),
+            "qtype": "nodes",
+        })
+    })
+    r.GET("/supernodes/:key", func(c *gin.Context) {
+        c.JSON(200, gin.H{
+            "key": c.Param("key"),
+            "qtype": "supernodes",
+        })
+    })
+    r.Run("localhost:" + port)
 }
