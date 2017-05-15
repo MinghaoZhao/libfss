51d1b196e90c16be68dd63a239e7110ea2ba1c16
Cathie Yun <cath_yun@mit.edu>
2017-05-14 22:53:11 -0400

    created map from transit nodes to nodes in their zone

--- webfss/fss_server/routing_data/gen-graph.go
+++ webfss/fss_server/routing_data/gen-graph.go
@@ -1,4 +1,12 @@
-  package main
+/*
+Take 3 parameters as input:
+1) the file of the travel data for that region (from DIMACS site)
+2) the file of the transit nodes for that region (from TRANSIT code)
+3) the file you want to output zone data to
+4) the file you want to output transit node shortest paths to
+*/
+
+package main
 
 import (
   "os"
@@ -6,20 +14,35 @@ import (
   "strings"
   "github.com/gyuho/goraph"
   "strconv"
+  "fmt"
+  "log"
 )
 
 func main() {
-  loc := "NY"
+  if len(os.Args) < 5 {
+    fmt.Println("Missing parameter(s), provide file names!")
+    return
+  }
 
-  // read in edges
-  edge_data, _ := os.Open("./"+loc+"-travel-data.txt")
-  defer edge_data.Close()
-  edge_reader := bufio.NewScanner(edge_data)
+  g := makeGraph(os.Args[1])
+  fmt.Println("Finished creating graph")
+  tnMap := getTransitNodes(os.Args[2], g)
 
-  // create graph from edges
+  gcopy := makeGraph(os.Args[1]) // TODO: copy g instead
+  getZones(os.Args[3], tnMap, gcopy) 
+}
+
+func makeGraph(travelDataFile string) goraph.Graph{
+  // read in edges from file
+  travel_data, err := os.Open(travelDataFile)
+  checkErr(err)
+  defer travel_data.Close()
+  travel_reader := bufio.NewScanner(travel_data)
+
+  // create graph from edges, using goraph
   g := goraph.NewGraph()
-  for edge_reader.Scan() {
-    edge := strings.Split(edge_reader.Text(), " ")
+  for travel_reader.Scan() {
+    edge := strings.Split(travel_reader.Text(), " ")
     if edge[0] == "a" {
       node1, err := g.GetNode(goraph.StringID(edge[1]))
       if err != nil {
@@ -35,50 +58,75 @@ func main() {
       g.AddEdge(node1.ID(), node2.ID(), weight)
     }
   }
-  fmt.Println("Finished creating graph")
+  return g
+}
 
+func getTransitNodes(transitNodeFile string, g goraph.Graph) map[goraph.Node]([]goraph.Node) {
   // read in transit nodes
-  tn_data, _ := os.Open("./"+loc+"-transit-nodes.txt")
+  tn_data, err := os.Open(transitNodeFile)
+  checkErr(err)
   defer tn_data.Close()
   tn_reader := bufio.NewScanner(tn_data)
-  
-  *gcopy := *g
-  var m map[goraph.Node]([]goraph.Node)
+
+  tnMap := make(map[goraph.Node]([]goraph.Node))
   for tn_reader.Scan() { 
-    tn, err := goraph.GetNode(tn_reader.Text())
-    
+    tn, err := g.GetNode(goraph.StringID(tn_reader.Text()))
+    checkErr(err)
+    tnMap[tn] = []goraph.Node{}
+  }
+  return tnMap
+}
+
+func getZones(loc string, tnMap map[goraph.Node]([]goraph.Node), gcopy goraph.Graph) {
+  // make queue for new nodes to expand
+  type Next struct {
+    enode goraph.Node // node to expand
+    tnode goraph.Node // transit node enode corresponds to
+    edge float64      // 
+  }
+  queue := make(chan Next, gcopy.GetNodeCount())
+
+  // populate queue with existing transit nodes
+  for tn, _ := range tnMap {
+    queue <- Next{tn, tn, 0}
   }
 
-
-/**
-  writef, _ := os.Create("./"+loc+"-join-data.txt")
-  writer := bufio.NewWriter(writef)
-
-  for edge_reader.Scan() {
-    line := strings.Split(creader.Text(), " ")
-    if line[0] == "p" {
-      writer.WriteString(line[4]+"\n")
-    } else if line[0] == "v" {
-      writer.WriteString(strings.Join(line[1:]," ")+"\n")
+  // repeat until all nodes have been added to zones (gcopy is empty)
+  for (gcopy.GetNodeCount() != 0) {
+    next := <- queue
+    // Check that target node is in gcopy. If not, skip.
+    enode, err := gcopy.GetNode(next.enode.ID())
+    if (err == nil) {
+      fmt.Println("adding node: ", next.enode, " to zone for tn: ", next.tnode)
+      // add target node to transit node zone mapping
+      tnMap[next.tnode] = append(tnMap[next.tnode], enode)
+      // get the children of target node, and add them to queue
+      targetsMap, err := gcopy.GetTargets(next.enode.ID())
+      checkErr(err)
+      for _, targetNode := range targetsMap {
+        queue <- Next{targetNode, next.tnode, 0}
+      }
+      // delete target node from gcopy
+      gcopy.DeleteNode(next.enode.ID())
+    } else {
+      fmt.Println("Node ", next.enode, " has already been handled.")
     }
   }
+  fmt.Println(tnMap)
+}
 
-  for dreader.Scan() {
-    treader.Scan()
-    dline := strings.Split(dreader.Text(), " ")
-    tline := strings.Split(treader.Text(), " ")
-    if dline[0] == "p" {
-      writer.WriteString(dline[3]+"\n")
-    } else if dline[0] == "a" {
-      top, _ := strconv.Atoi(dline[3])
-      bottom, _ := strconv.Atoi(tline[3])
-      speed := (top*15)/bottom
-      writer.WriteString(strings.Join(dline[1:]," ")+" 1 "+strconv.Itoa(speed)+"\n")
-      fmt.Println(speed)
+func checkErr(e error) {
+  if e != nil {
+    log.Fatal(e)
+  }
+}
+
+func printMap(tnMap map[goraph.Node]([]goraph.Node)) {
+  for tn, nodes := range tnMap {
+    fmt.Printf("\n"+tn.String()+": ")
+    for _, node := range nodes {
+      fmt.Printf(node.String()+ " ")
     }
   }
-
-  writer.Flush()
-**/
-
+  fmt.Printf("\n")
 }
